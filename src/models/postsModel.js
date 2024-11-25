158c20e3bbd2b532b4a1cae119753f8147e17339
gabrielprimieri <gabriel_primieri@hotmail.com>
2024-11-25 13:11:50 -0300

    Aula04 - Criando rotas  POST/post e POST/upload; Enviando informações para a base de dados; Implementando o upload de imagens e testando a API com  thunderClient e Postman.

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -3,12 +3,18 @@ import conectarAoBanco from "../config/dbconfig.js"
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 // Função assíncrona para buscar todos os posts do banco de dados.
-export default async function getTodosPosts() {
+export async function getTodosPosts() {
     // Seleciona o banco de dados 'imersao-instabytes'.
     const db = conexao.db("imersao-instabytes");
     // Seleciona a coleção 'posts' dentro do banco de dados.
     const colecao = db.collection("posts");
     // Executa uma consulta para encontrar todos os documentos (posts) e retorna um array com todos os documentos da coleção
     return colecao.find().toArray();
+  }
+
+  export async function criarPost(novoPost) {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost)
   }
   
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,11 +1,35 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
 
+// Configura o armazenamento de arquivos utilizando o Multer
+const storage = multer.diskStorage({
+  // Define o destino onde os arquivos serão salvos (ajuste 'uploads/' para sua necessidade)
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Callback com null para erro e 'uploads/' como destino
+  },
+  // Define o nome do arquivo (considere usar identificadores únicos ou extensões)
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Callback com null para erro e nome original do arquivo
+  }
+});
+
+// Cria uma instância do Multer com a configuração de armazenamento
+const upload = multer({ storage });
+
+// Define as rotas da aplicação
 const routes = (app) => {
   // Permite que o servidor interprete requisições com corpo no formato JSON
   app.use(express.json());
-  // Rota GET para a URL '/posts', para buscar todos os posts
+
+  // Rota GET para '/posts' - Busca todos os posts (implementação em listarPosts)
   app.get("/posts", listarPosts);
+
+  // Rota POST para '/posts' - Cria um novo post (implementação em postarNovoPost)
+  app.post("/posts", postarNovoPost);
+
+  // Rota POST para '/upload' - Envia uma imagem (implementação em uploadImagem)
+  app.post("/upload", upload.single("imagem"), uploadImagem);
 };
 
 export default routes;
